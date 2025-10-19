eae78582bb582c9147293c2228aef4d3b35b6d2d
ramp-member <v279136@gmail.com>
2025-10-20 01:27:59 +0300

    Add files via upload

---
+++ 7.2.c
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+void calculator() {
+    float x, y;
+    char op;
+    printf("Введите выражение: ");
+    scanf("%f%c%f", &x, &op, &y);
+
+    switch (op) {
+    case '+':
+        printf("= %.2f\n", x + y);
+        break;
+    case '-':
+        printf("= %.2f\n", x - y);
+        break;
+    case '*':
+        printf("= %.2f\n", x * y);
+        break;
+    case '/':
+        if (y != 0)
+            printf("= %.2f\n", x / y);
+        else
+            printf("Ошибка: деление на ноль!\n");
+        break;
+    }
+}
+
+int main() {
+    setlocale(LC_CTYPE, "RUS");
+    calculator();
+    return 0;
+}
