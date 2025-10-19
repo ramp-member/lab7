d3af512e057e7111a452ac14a174d07fe3cd9d46
ramp-member <v279136@gmail.com>
2025-10-20 01:23:06 +0300

    Add files via upload

---
+++ 7.1.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+void simvol() {
+    char c;
+
+    printf("Введите один символ: ");
+    scanf(" %c", &c);
+
+    switch (c) {
+    case '0': case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '8': case '9':
+        printf("Это цифра.\n");
+        break;
+
+    default:
+        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+            printf("Это буква.\n");
+        else
+            printf("Неизвестный символ.\n");
+        break;
+    }
+}
+
+int main() {
+    setlocale(LC_CTYPE, "RUS");
+    simvol();
+    return 0;
+}
